4475aaf32f5cf7483f002283285a7b3399a3e7c0
DeadlyPants11 <ostrjanin.rostik@gmail.com>
2024-02-21 20:06:38 +0200

    scrol

---
+++ js/header-scrol.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", function () {
+  let header = document.getElementById("header");
+  if (window.scrollY > 0) {
+    header.classList.add("header-scrolled");
+  } else {
+    header.classList.remove("header-scrolled");
+  }
+});
